7d031985cd0c1c01c3e2aa45e15894c2b9cc6ae8
Birkir K <birkir17@ru.is>
2019-03-06 17:59:12 +0000

    gerdi service fyrir get single bubble

--- template/src/services/bubbleService.js
+++ template/src/services/bubbleService.js
@@ -8,4 +8,14 @@ const getAllBubbles = () => {
     });
 };
 
+const getSingleBubble = id => {
+    fetch('http://localhost:3500/api/bubbles/' + id).then(resp => {
+        if (resp.ok) { return resp.json(); }
+    }).then(data => {
+        if(!data) {return [];}
+        const bubbles = data.map(d => { return { id: d.id, name: d.name, description: d.description, image: d.image, price: d.price} });
+        return bubbles;
+    });
+};
+
 export default {getAllBubbles};
